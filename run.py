d8841d454a0ae72beb7922a59d7a55fb1d298c05
tmarkec <tmarkec@gmail.com>
2023-01-23 10:15:23 +0000

    add function to choose level

--- run.py
+++ run.py
@@ -7,8 +7,9 @@ from hangman import hangman_as
 
 
 def welcome():
-    """Welcome screen which will provide user
-       with rules and input field for his name
+    """
+    Function to welcome screen which will provide user
+    with rules and input field for his name.
     """
     ascii_banner = pyfiglet.figlet_format("Welcome   to   HANGMAN!!!")
     print(ascii_banner)
@@ -21,19 +22,51 @@ def welcome():
     print("\n")
 
     while True:
-        name = input("Enter your name:\n")
+        name = input("Please enter your name:\n").capitalize()
+
         if not name.isalpha():
-            print("Name must be alphabets only\n")
+            print("Name must be alphabets only!!!\n")
+            
         else:
+            clear()
+            levels()
             break
     return name
 
 
 def clear():
-    """Clears the screen through the code
+    """
+    Function to clear screen through the code.
     """
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
+def levels():
+    """
+    This function will ask player to choose dificult level for the game.
+    """
+    clear()
+    print('Hello lets pick level for you!!!')
+    choose = False
+    lives = 0
+    while choose is False:
+        difficulty = input('Enter E for easy, M for medium or H for hard\n')\
+            .upper()
+        if difficulty == 'E':
+            choose = True
+            lives = 6
+            return lives
+        elif difficulty == 'M':
+            choose = True
+            lives = 4
+            return lives
+        elif difficulty == 'H':
+            choose = True
+            lives = 2
+            return lives
+        else:
+            print('Please type E, M or H to choose dificulty levels!!!')
+            choose = False
+
+
 welcome()
-clear()
