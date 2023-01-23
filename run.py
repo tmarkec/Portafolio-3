b973fe5b001f1ceffad46ae919ddda4d083da76c
tmarkec <tmarkec@gmail.com>
2023-01-23 09:29:11 +0000

    fix bug with validating username

--- run.py
+++ run.py
@@ -17,8 +17,23 @@ def welcome():
     print("that makes hidden word. With each wrong")
     print("guess you are one step closer to beeing")
     print("hanged! You can choose between 3 levels")
-    print("but for start lets get your name first")
+    print("but for start lets get your name first....")
     print("\n")
+
+    while True:
+        name = input("Enter your name:\n")
+        if not name.isalpha():
+            print("Name must be alphabets only\n")
+        else:
+            break
+    return name
+
+
+def clear():
+    """Clears the screen through the code
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 welcome()
+clear()
