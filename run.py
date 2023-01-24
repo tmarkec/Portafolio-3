2aabf4b1447818648e771e5c048d2e527187d530
tmarkec <tmarkec@gmail.com>
2023-01-24 10:59:11 +0000

    install colour & text package, add style to text

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 import random
 import string
 import os
-import pyfiglet
+from colorama import Fore
+from colorama import init
 from pyfiglet import Figlet
 from hangman import hangman_as
 from words import word_list
+init(autoreset=True)
 
 
 def welcome():
@@ -12,8 +14,8 @@ def welcome():
     Function to welcome screen which will provide user
     with rules and input field for his name.
     """
-    ascii_banner = pyfiglet.figlet_format("Welcome to HANGMAN!")
-    print(ascii_banner)
+    welcome_text = Figlet(font='sblood')
+    print(welcome_text.renderText('Welcome to Hangman'))
     print("\n")
     print("Rules of this game are fairly simple, you are")
     print("guessing letters one by one that makes the hidden word.")
@@ -21,13 +23,13 @@ def welcome():
     print("hanged! You can choose different levels of difficulty!")
     print("But let's not skip the steps now, first enter your name!")
     print("\n")
-
     global name
+
     while True:
         name = input("Please enter your name:\n").strip().capitalize()
 
         if not name.isalpha():
-            print("Name must be alphabets only!!!\n")
+            print(Fore.RED + "Name must be alphabets only!!!\n")
             
         else:
             clear()
@@ -37,7 +39,7 @@ def welcome():
 
 def clear():
     """
-    Function to clear screen through the code.
+    Function to clear terminal through the game.
     """
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -53,11 +55,9 @@ def get_word(word_list):
 
 def levels():
     """
-    This function will ask player to choose dificult level for the game.
+    This function will ask player to choose dificulty level for the game.
     """
     clear()
-    ascii_title = pyfiglet.figlet_format("HANGMAN!!!")
-    print(ascii_title)
     print('\n')
     print(f'What a lovely name {name}, lets pick level for you!!!')
     print('\n')
@@ -77,11 +77,20 @@ def levels():
                 raise ValueError(f'Please {name} type E or M or H for difficulty level!\n')
         except ValueError as e_rr:
             print(f"Invalid input:{e_rr}")
-        
-    return lives
 
+    clear()  
+    return lives
+    
 
 def game():
+    """
+    This function will run the game, sets lives and 
+    template for player to play depends on the level he choose.
+    The game will finish when user either guess the word or
+    loose all lives.
+
+    """
+    clear()
     word = get_word(word_list)
     hidden_word = set(word)
     letter_alphab = set(string.ascii_uppercase)
@@ -89,34 +98,30 @@ def game():
     tries = levels()
 
     while len(hidden_word) > 0 and tries > 0:
-        print(f'{name} you have {tries} lives mleft for this round')
+        print(f'{name} you have {tries} lives left for this round')
         print('You used:', ' '.join(used_letters))
         letter_words = [letter if letter in used_letters else '-' for letter in word]
 
         print(hangman_as[tries])
         print('Current word is:', ' '.join(letter_words))
 
-        user_guess = input('Try to guess letter: \n').upper()
-
+        user_guess = input('\nTry to guess letter: \n').upper()
+        clear()
         if user_guess in letter_alphab - used_letters:
-            clear()
             used_letters.add(user_guess)
             if user_guess in hidden_word:
-                clear()
                 hidden_word.remove(user_guess)
             else:
-                clear()
                 tries -= 1
-                print('Your guess is not in the work, try again!')
-        elif user_guess in used_letters and len(user_guess) != 1:
-            clear()
-            print('You used this letter already, try again')
+                print(Fore.RED + 'Your guess is not in the word, try again!')
+        elif user_guess in used_letters:
+             print(Fore.YELLOW + 'You used this letter already, try again')
         else:
-            clear()
-            print('Unrecognized character, please try again with letter!')
+            print(Fore.RED + 'Unrecognized character, please try again with letter!')
+            
     if tries == 0:
         clear()
-        custom_fig = Figlet(font='graffiti')
+        custom_fig = Figlet(font='doom')
         print(custom_fig.renderText(f'Sorry {name} you lost!!!'))
         print(f'The word we were looking for was {word}')
         print(hangman_as[tries])
@@ -124,13 +129,16 @@ def game():
 
     else:
         clear()
-        print(f'Well done {name}, you won!!!')
+        text_win = Figlet(font='doom')
+        print(text_win.renderText(f'Well done {name}, you won!!!'))
         end()
-
+    
 
 def end():
-    """ Function that will ask user if he wants to play again.
+    """ 
+    Function that will ask user if he wants to play again.
     """
+
     while True:
         again = input('Would you like to play again? \n Y/N?\n').upper()
         try:
@@ -143,26 +151,19 @@ def end():
                 thank_you()
                 break
             else:
-                raise ValueError(f'You must type Y or N. You typed {again}')
+                raise ValueError(Fore.RED + '\nYou must type Y or N.')
         except ValueError as e:
             print(f'Try again: {e}')
 
 
 def thank_you():
+    """
+    This function will just print text for user 
+    when he decided to stop playing game
+    """
     clear()
-    print(
-        """
-         ,--.--------.  ,--.-,,-,--,   ,---.      .-._        ,--.-.,-.                         _,.---._                 
-        /==/,  -   , -\/==/  /|=|  | .--.'  \    /==/ \  .-._/==/- |\  \        ,--.-.  .-,--.,-.' , -  `.  .--.-. .-.-. 
-        \==\.-.  - ,-./|==|_ ||=|, | \==\-/\ \   |==|, \/ /, /==|_ `/_ /       /==/- / /=/_ //==/_,  ,  - \/==/ -|/=/  | 
-        `--`\==\- \   |==| ,|/=| _| /==/-|_\ |  |==|-  \|  ||==| ,   /        \==\, \/=/. /|==|   .=.     |==| ,||=| -| 
-            \==\_ \  |==|- `-' _ | \==\,   - \ |==| ,  | -||==|-  .|          \==\  \/ -/ |==|_ : ;=:  - |==|- | =/  | 
-            |==|- |  |==|  _     | /==/ -   ,| |==| -   _ ||==| _ , \          |==|  ,_/  |==| , '='     |==|,  \/ - | 
-            |==|, |  |==|   .-. ,\/==/-  /\ - \|==|  /\ , |/==/  '\  |         \==\-, /    \==\ -    ,_ /|==|-   ,   / 
-            /==/ -/  /==/, //=/  |\==\ _.\=\.-'/==/, | |- |\==\ /\=\.'         /==/._/      '.='. -   .' /==/ , _  .'  
-            `--`--`  `--`-' `-`--` `--`        `--`./  `--` `--`               `--`-`         `--`--''   `--`..---'   
-      """
-    )
+    thank_text = Figlet(font='doom')
+    print(thank_text.renderText(f'Thank you for playing this game {name}!'))
 
 
 welcome()
